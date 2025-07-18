65addfff33c117874120c0c6ff6593b57b3f4989
THakkal <salahhakkal50@gmail.com>
2025-07-18 19:09:37 +0200

    18/07/2025

--- Romy/src/FirstPage/App.tsx
+++ Romy/src/FirstPage/App.tsx
@@ -23,7 +23,7 @@ function App() {
         <Link to="/about" >
           <img src={icons8} className="logo" alt="Icons8 logo" />
         </Link>
-        <a href="CreationCompte" target="_blank">
+        <a href="Connexion" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
